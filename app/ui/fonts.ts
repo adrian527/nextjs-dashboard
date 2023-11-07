462bfcc8d9fdf40496484a27bb2bb729da3e92dd
Adrian Gzyl <adig2158@gmail.com>
2023-11-07 11:32:47 +0100

    Initialize repo

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Roboto } from 'next/font/google';
+import {Lusitana } from 'next/font/google';
+
+export const roboto = Roboto({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
